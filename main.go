b954b65c443f26a4cc223832399e5f392c12ddf9
Ernest Mallett <ernest.mallett@dccoder.com>
2025-10-23 15:42:16 -0500

    Update program to use skyfall

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	"github.com/BlueMonday/go-scryfall"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	ctx := context.Background()
+	client, err := scryfall.NewClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sco := scryfall.SearchCardsOptions{
+		Unique:              scryfall.UniqueModePrints,
+		Order:               scryfall.OrderSet,
+		Dir:                 scryfall.DirDesc,
+		IncludeExtras:       false,
+		IncludeMultilingual: false,
+		IncludeVariations:   false,
+	}
+	result, err := client.SearchCards(ctx, "sliver", sco)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%s", result.Cards[0].Colors)
 }
